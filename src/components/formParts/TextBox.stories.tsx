cf0c4e0120b354115fd8f90a5ad850758697da59
paibamboo <gavinsitti@hotmail.com>
2019-10-04 09:43:06 +0700

    style: fix lint

--- src/components/formParts/TextBox.stories.tsx
+++ src/components/formParts/TextBox.stories.tsx
@@ -8,14 +8,14 @@ export default {
 };
 
 export const Normal = ({state, setState}) => (
-  <TextBox value={state.value} onChange={(text) => {setState({value: text});}}/>
+  <TextBox value={state.value} onChange={(text) => setState({value: text})}/>
 );
 Normal.story = {
   decorators: [withInitialState({value: ""})]
 };
 
 export const WithError = ({state, setState}) => (
-  <TextBox value={state.value} onChange={(text) => {setState({value: text});}} error={"Error occurred!"}/>
+  <TextBox value={state.value} onChange={(text) => setState({value: text})} error={"Error occurred!"}/>
 );
 WithError.story = {
   decorators: [withInitialState({value: ""})]
