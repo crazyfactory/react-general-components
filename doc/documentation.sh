873f69ec8eb0e6ee3794062eff95348015181878
paibamboo <gavinsitti@hotmail.com>
2019-10-15 13:13:33 +0700

    ci: should post comment on react-general-components repo

--- doc/documentation.sh
+++ doc/documentation.sh
@@ -8,11 +8,11 @@ if [ -z "$TRAVIS_TAG" ]; then
   npm run doc:publish $TRAVIS_BRANCH
   export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
   curl -X POST \
-    https://api.github.com/repos/crazyfactory/shop-webapp-components/issues/$TRAVIS_PULL_REQUEST/comments \
+    https://api.github.com/repos/crazyfactory/react-general-components/issues/$TRAVIS_PULL_REQUEST/comments \
     -H "authorization: token $GH_TOKEN" \
     -H 'content-type: application/json' \
     -d "{
-    \"body\": \"Hi there, the generated documentation has been made available [gh-pages/$BRANCH](https://crazyfactory.github.io/shop-webapp-components/$BRANCH). Don't forget to delete from [gh-pages/](https://github.com/crazyfactory/shop-webapp-components/tree/gh-pages/) branch once this PR is merged!\"
+    \"body\": \"Hi there, the generated documentation has been made available [gh-pages/$BRANCH](https://crazyfactory.github.io/react-general-components/$BRANCH). Don't forget to delete from [gh-pages/](https://github.com/crazyfactory/react-general-components/tree/gh-pages/) branch once this PR is merged!\"
   }"
 else
   echo "it's a tag, publishing main document"
